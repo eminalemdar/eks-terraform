18150b7e248e65c7fe5955cc07cee9b4201676d0
emnalmdr <56020356+emnalmdr@users.noreply.github.com>
2021-03-16 18:30:40 +0300

    Updated outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -27,8 +27,3 @@ output "region" {
   description = "AWS region"
   value       = var.region
 }
-
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = local.cluster_name
-}
